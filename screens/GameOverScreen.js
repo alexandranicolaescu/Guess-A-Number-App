6d75d844fb7f6a06ec0f5ba72e975ded262ca37e
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-06-05 16:34:06 +0300

    StartGame + GameOver Buttons/ Create Custom Button

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
+import MainButton from '../components/MainButton';
 
 const GameOverScreen = props => {
     return (
@@ -19,14 +20,17 @@ const GameOverScreen = props => {
                 />
             </View>
             <View style = {styles.resultContainer}>
-            <BodyText> 
-                Your phone needed 
+            <BodyText style = {styles.resultText}> 
+                I only needed
                 <Text style = {styles.highlight}> {props.roundsNumber} </Text> 
-                rounds to guess the number 
-                <Text style = {styles.highlight}> {props.userNumber} </Text> 
+                rounds to guess the number
+                <Text style = {styles.highlight}> {props.userNumber}, </Text> 
+                haha!
             </BodyText>
             </View>
-            <Button title = "NEW GAME" onPress = {props.onRestart} />
+            <MainButton  onPress = {props.onRestart}>
+            NEW GAME
+            </MainButton>
         </View>
     );
 };
@@ -54,11 +58,19 @@ const styles = StyleSheet.create({
     },
 
     highlight: {
-        color:Colors.primary
+        color:Colors.primary,
+        fontFamily: 'open-sans-bold',
+    
     },
 
     resultContainer: {
-       width: '80%',
+       marginHorizontal: 30,
+       marginVertical: 15
+    },
+
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
     }
 });
 
