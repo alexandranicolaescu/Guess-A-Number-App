bc11484cde21fff7a1a0fc61f52275769fc867e4
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-06-08 18:37:43 +0300

    EventListener to change the info on height and width

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    Button, 
+    Image, 
+    Dimensions,
+    ScrollView 
+} from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
@@ -8,30 +16,32 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = props => {
     return (
-        <View style = {styles.screen} >
-            <TitleText> The Game is Over! </TitleText>
-            <View style = {styles.imageContainer} >
-                <Image 
-                    fadeDuration = {300}
-                    source = {require('../assets/success.png')}           //local image
-                    //source = {{uri: 'https://images.wallpaperscraft.com/image/mountains_night_northern_lights_148100_1440x900.jpg' }}             //web image
-                    style = {styles.image} 
-                    resizeMode = "cover"
-                />
+        <ScrollView>
+            <View style = {styles.screen} >
+                <TitleText> The Game is Over! </TitleText>
+                <View style = {styles.imageContainer} >
+                    <Image 
+                        fadeDuration = {300}
+                        source = {require('../assets/success.png')}           //local image
+                        //source = {{uri: 'https://images.wallpaperscraft.com/image/mountains_night_northern_lights_148100_1440x900.jpg' }}             //web image
+                        style = {styles.image} 
+                        resizeMode = "cover"
+                    />
+                </View>
+                <View style = {styles.resultContainer}>
+                <BodyText style = {styles.resultText}> 
+                    I only needed
+                    <Text style = {styles.highlight}> {props.roundsNumber} </Text> 
+                    rounds to guess the number
+                    <Text style = {styles.highlight}> {props.userNumber}, </Text> 
+                    haha!
+                </BodyText>
+                </View>
+                <MainButton onPress = {props.onRestart}>
+                NEW GAME
+                </MainButton>
             </View>
-            <View style = {styles.resultContainer}>
-            <BodyText style = {styles.resultText}> 
-                I only needed
-                <Text style = {styles.highlight}> {props.roundsNumber} </Text> 
-                rounds to guess the number
-                <Text style = {styles.highlight}> {props.userNumber}, </Text> 
-                haha!
-            </BodyText>
-            </View>
-            <MainButton onPress = {props.onRestart}>
-            NEW GAME
-            </MainButton>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -48,13 +58,13 @@ const styles = StyleSheet.create({
     },
 
     imageContainer: {
-        height: 300,
-        width: 300,
-        borderRadius: 150,
+        height: Dimensions.get('window').width * 0.7,
+        width: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: 30
+        marginVertical: Dimensions.get('window').height / 30
     },
 
     highlight: {
@@ -65,12 +75,12 @@ const styles = StyleSheet.create({
 
     resultContainer: {
        marginHorizontal: 30,
-       marginVertical: 15
+       marginVertical: Dimensions.get('window').height / 60
     },
 
     resultText: {
         textAlign: 'center',
-        fontSize: 20
+        fontSize: Dimensions.get('window').height < 400 ? 16 : 20
     }
 });
 
