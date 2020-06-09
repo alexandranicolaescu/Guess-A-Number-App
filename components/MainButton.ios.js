e43f0473fca7284557daa393e06a6fc30d595c14
Mon Alexandra <alexandramonicanicolaescu@gmail.com>
2020-06-09 11:14:35 +0300

    Changes for Android and IOS platform

--- components/MainButton.ios.js
+++ components/MainButton.ios.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableOpacity
+} from 'react-native';
 
 import Colors from '../constants/colors';
 
 const MainButton = props => {
     return (
-        <TouchableOpacity 
-            activeOpacity = {0.8} 
-            onPress = {props.onPress} >
-            
-            <View style = {styles.button} >
-                <Text style = {styles.buttonText}> {props.children} </Text>
-            </View>
+            <TouchableOpacity
+                activeOpacity = {0.8} 
+                onPress = {props.onPress} >
+                
+                <View style = {styles.button} >
+                    <Text style = {styles.buttonText}> {props.children} </Text>
+                </View>
 
-        </TouchableOpacity>
+            </TouchableOpacity>
     );
 };
 
